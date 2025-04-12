14822fd2a69b53fbcd0e7dd38a2cdd7f014b2d08
antiching <taja526526@gmail.com>
2025-04-12 15:03:01 +0800

    feat: three way API

---
+++ FileSystem/threeWay.js
@@ -0,0 +1,22 @@
+// Promise API
+
+const fsPromise = require('fs/promises');
+
+(async () => {
+    try {
+        await fsPromise.copyFile('example.txt', 'copied_example_promise.txt')
+    } catch (e) {
+        console.log(e)
+    }
+})()
+
+
+// Callback API
+const fs = require('fs')
+
+fs.copyFile('example.txt', 'copied_example_cb.txt', (err) => { // cb first param is error
+    console.log(err)
+})
+
+// Syc API
+fs.copyFileSync('example.txt', 'copied_exaple_sync.txt')
