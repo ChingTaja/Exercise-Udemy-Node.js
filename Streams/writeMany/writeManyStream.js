93ae03d1c6c8aac39c8c421e8c4a9282733a1eb9
antiching <taja526526@gmail.com>
2025-04-26 01:20:10 +0800

    feat: Stream

--- Streams/writeMany/writeManyStream.js
+++ Streams/writeMany/writeManyStream.js
@@ -1,1 +1,32 @@
 // 加速 ~~
+
+// Stream 
+
+///Execution Time: 600ms (快!)
+// CPU Usage: 55% (one core)
+// Memory Usage: 
+
+const { open } = require('node:fs/promises');
+
+const writeMany = async () => {
+    console.time('write many')
+    let fileHandle = await open('./text.txt', 'w')
+
+    // 只是範例 , 不要這樣子寫 !! 會用掉很多 memory
+    const stream = fileHandle.createWriteStream()
+    try {
+        for (let i = 0; i < 100000; i++) {
+            const buffer = Buffer.from(`${i} `, 'utf-8')
+
+            stream.write(buffer)
+        }
+
+        await fileHandle.close();
+        console.log('寫入完成');
+    } catch (e) {
+        console.log('An error occurred', e)
+    }
+    console.timeEnd('write many')
+}
+
+writeMany()
