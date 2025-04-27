5fa8f53000b3831c40ffef2d526815795711bf3d
antiching <taja526526@gmail.com>
2025-04-27 14:59:29 +0800

    refactor: stream

--- Streams/writeMany/writeManyStream2.js
+++ Streams/writeMany/writeManyStream2.js
@@ -1,40 +1,61 @@
 // Stream 
 
 const { open } = require('node:fs/promises');
+const { resolve } = require('node:path');
 
-const writeMany = async () => {
-    console.time('write many')
+
+(async () => {
+
     let fileHandle = await open('./text.txt', 'w')
-
-
     const stream = fileHandle.createWriteStream()
-    const buffer = Buffer.alloc(16383, 10)
-
-    // ! 避免讓 writableLength 大於 writableHighWaterMark ! 
-    console.log(stream.writableHighWaterMark) // internal buffer max watermark 16384
-    console.log(stream.writableLength) // internal buffer
-    console.log(stream.write(buffer))
-    console.log(stream.write(Buffer.alloc(1, "a")))
-    console.log(stream.write(Buffer.alloc(1, "a")))
-
-    stream.on('drain', () => {
-        console.log('Internal Buffer 之前滿了又被清空了, 可以放東西進來了')
-    })
 
 
-    try {
-        // for (let i = 0; i < 100000; i++) {
-        //     const buffer = Buffer.from(`${i} `, 'utf-8')
+    console.time('write many')
+    let i = 0
+    const writeMany = async () => {
 
-        //     stream.write(buffer)
-        // }
 
-        // await fileHandle.close();
-        console.log('寫入完成');
-    } catch (e) {
-        console.log('An error occurred', e)
+        /* 避免讓 writableLength 大於 writableHighWaterMark!!!
+        const buffer = Buffer.alloc(16383, 10)
+    
+    
+        console.log(stream.writableHighWaterMark) // internal buffer max watermark 16384
+        console.log(stream.writableLength) // internal buffer
+        console.log(stream.write(buffer))
+        console.log(stream.write(Buffer.alloc(1, "a")))
+        console.log(stream.write(Buffer.alloc(1, "a")))
+    
+        stream.on('drain', () => {
+            console.log('Internal Buffer 之前滿了又被清空了, 可以放東西進來了')
+        })
+    
+        */
+        while (i < 10000) {
+            const buffer = Buffer.from(`${i} `, 'utf-8')
+            if (i === 9999) {
+                return stream.end(buffer)
+                // stream.end 後面再 stream.write 會報錯
+            }
+
+
+            let isFilled = stream.write(buffer)
+
+            i++
+            // if internal buffer is full, stop the loop
+            if (!isFilled) break
+        }
+
+        writeMany() // 寫在事件 finish 後會報錯 write after end
+
+        // resume loop when internal buffer is empty
+        stream.on('drain', () => {
+            console.log('Internal Buffer 之前滿了又被清空了, 可以放東西進來了')
+            writeMany()
+        })
+
+        stream.on('finish', () => {
+            fileHandle.close()
+            console.timeEnd('write many')
+        })
     }
-    console.timeEnd('write many')
-}
-
-writeMany()
+})()
