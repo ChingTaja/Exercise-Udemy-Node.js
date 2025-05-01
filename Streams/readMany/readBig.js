c87ec91865df03cde36cddb85e00de3e6161f584
antiching <taja526526@gmail.com>
2025-05-01 23:25:01 +0800

    fix: linter

--- Streams/readMany/readBig.js
+++ Streams/readMany/readBig.js
@@ -14,10 +14,8 @@ const { open } = require('node:fs/promises');
     // Readable Stream data event
     streamRead.on("data", (chunk) => {
         // Readable Stream - internal buffer max watermark 是 64kb (64 * 1024)
-        console.log(chunk.length)
 
         const numbers = chunk.toString("utf-8").split("  ")
-        console.log(numbers)
 
         // FIX: 陣列的 頭部 尾部被切掉
         // 原因: (每個 chunk 有 watermark , 最後一個  item 有可能被 切一半 , 下一個 chunk 再放進來)
@@ -25,23 +23,39 @@ const { open } = require('node:fs/promises');
 
         // if (第一個 + 1) 不等於 第二個 -> 出事了
         if (Number(numbers[0]) !== Number(numbers[1] - 1)) {
-            // fix first item (trim 去空格 , 返回新字串)
-            numbers[0] = splitItem.trim() + numbers[0].trim()
+
+            if (numbers[0] === '') {
+                splitItem = ''
+                numbers.shift()
+            }
+
+            if (splitItem) {
+                // fix first item (trim 去空格 , 返回新字串)
+                numbers[0] = splitItem.trim() + numbers[0].trim()
+            }
         }
 
         // if (倒數第二個 + 1) 不等於 (倒數最後一個) -> 出事了
         if ((Number(numbers[numbers.length - 2]) + 1) !== Number(numbers[numbers.length - 1])) {
-            splitItem = numbers.pop()
+            splitItem = numbers.pop().trim()
         }
 
-
-        // if writable stream is full,pause the readable stream 
-        if (!streamWrite.write(chunk)) {
-            streamRead.pause()
-        }
-
-        streamWrite.on("drain", () => {
-            streamRead.resume()
+        numbers.forEach((number) => {
+            let n = Number(number)
+            if (n % 2 === 0) {
+                // if writable stream is full,pause the readable stream
+                if (!streamWrite.write(" " + n + " ")) {
+                    streamRead.pause()
+                }
+            }
         })
+    })
+    streamWrite.on("drain", () => {
+        streamRead.resume()
+    })
+
+
+    streamRead.on('end', () => {
+        console.log()
     })
 })()
